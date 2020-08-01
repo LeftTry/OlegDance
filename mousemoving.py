a43c2957e5d6de163bf24853b1befdcdd2e62dd7
Иван Александрович Стахов <50211739+LeftTry@users.noreply.github.com>
2020-08-01 09:42:52 +0300

    Add files via upload

---
+++ mousemoving.py
@@ -0,0 +1,15 @@
+import time 
+import pyautogui as gui
+
+def moveCursor(x, y):
+	int a = 20, b = 20
+	currentMouseX, currentMouseY = gui.position()
+	while currentMouseX != x and currentMouseY != y:
+		print("yes")
+		currentMouseX, currentMouseY = gui.position()
+		gui.moveTo(currentMouseX + a, currentMouseY + b, 2)  
+		if(currentMouseX == x)
+			a = 0
+		if(currentMouseY == y)
+			b = 0
+moveCursor(100, 1000)
---
+++ spamterminals.bat
@@ -0,0 +1,4 @@
+@echo off
+echo FEED ME!
+echo FEED ME!
+pause
